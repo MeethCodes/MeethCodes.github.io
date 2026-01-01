88a9531488b4485a5d33e2224da06756122ec0f1
amin_meeth <68213420+MeethCodes@users.noreply.github.com>
2026-01-01 17:12:31 +0530

    Update resume link in Links constant

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,7 +2,7 @@ export const Links = {
     github: "https://github.com/MeethCodes",
     linkedin: "https://www.linkedin.com/in/meeth-amin-4a73711b3/",
     email: "aminmeeth89@gmail.com",
-    resume: "https://drive.google.com/file/d/1nQEoPSjotrhQz43dtP1e0LJtwuUAZFnW/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/1zuc-SGJjjEKkaxCTjb2pmTMLhl6HLS8d/view?usp=sharing",
     location: "Mumbai, India",
     phone: "+91 8450992775",
   };
